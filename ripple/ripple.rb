daf0a9d281fd01f6c8948ade06868c85b827e8a1
dave augustine <david.augustine@gmail.com>
2010-01-18 17:24:14 -0800

    refactoring. implementing single ripple source for now. ability to turn on / off origin

--- ripple/ripple.rb
+++ ripple/ripple.rb
@@ -1,16 +1,17 @@
 #!/usr/bin/env jruby -wKU
 
-require File.dirname(__FILE__) + '/../../lib/monomer'
+require File.dirname(__FILE__) + '/../../monomer/lib/monomer'
 
 class Ripple < Monomer::Listener
   
   before_start do
     @midi = Monomer::MidiOut.new
     @buttons_on = []
+    @origin = nil
     @lights_on = []    
     @max_steps = 8
     @current_step = 0
-    @play_debug = false    
+    @play_debug = true    
   end
 
   on_start do
@@ -18,45 +19,60 @@ class Ripple < Monomer::Listener
   end
   
   on_button_press do |x,y|
-    @buttons_on.push({:x => x, :y => y})
+    unless @origin.nil? or (@origin[:x] == x && @origin[:y] == y)
+      @buttons_on.push({:x => x, :y => y})
+    else
+      @origin = {:x => x, :y => y}
+    end    
   end
   
   def self.play_notes 
       
-      if @play_debug
-        print "Playing note at #{button[:x]}, #{button[:y]}\n"
-        puts test
-      end
-      
-      unless @current_step == @max_steps 
-        light_rectangle(@current_step)  
+      unless (@current_step == @max_steps) || @origin.nil?
+        monome.clear
+        light_origin
+        light_others        
+        light_square(@current_step)  
+        check_for_hit
         @current_step += 1
       else
         @current_step = 0
       end  
 
   end
-
-  def self.light_rectangle(size)
-    monome.clear
+  
+  def self.check_for_hit
+    @buttons_on.each do |button|  
+      if @lights_on.include?(button)
+        @midi.prepare_note(:duration => 0.4 * (60 / 120.0 / 4), :note => button[:x] * 8 + button[:y])
+      end
+    end
+          
+  end
+  
+  def self.light_origin
+    monome.toggle_led( @origin[:x], @origin[:y])    
+  end
+  
+  def self.light_others
+    for button in @buttons_on do
+      monome.toggle_led( button[:x], button[:y])
+    end    
+  end
+   
+  def self.light_square(size)
     @lights_on = []
-    
-    @buttons_on.each do |button|
-      monome.toggle_led(button[:x], button[:y])
-      
-      upper_left    = { :x => button[:x] - size, :y => button[:y] - size  }
-      upper_right   = { :x => button[:x] + size, :y => button[:y] - size  }
-      lower_left    = { :x => button[:x] - size, :y => button[:y] + size  }    
-      lower_right   = { :x => button[:x] + size, :y => button[:y] + size  }    
-    
+        
+    unless size == 0
+      upper_left    = { :x => @origin[:x] - size, :y => @origin[:y] - size  }
+      upper_right   = { :x => @origin[:x] + size, :y => @origin[:y] - size  }
+      lower_left    = { :x => @origin[:x] - size, :y => @origin[:y] + size  }    
+      lower_right   = { :x => @origin[:x] + size, :y => @origin[:y] + size  }    
+  
       light_line( upper_left  , upper_right )
       light_line( upper_right , lower_right )
       light_line( lower_left  , lower_right )
       light_line( upper_left  , lower_left  )
-      
-      if @lights_on.include?(button)
-        @midi.prepare_note(:duration => 0.4 * (60 / 120.0 / 4), :note => button[:x] * 8 + button[:y])
-      end
     end  
   end
 
@@ -72,6 +88,7 @@ class Ripple < Monomer::Listener
     else
       for y in starting[:y]..ending[:y] do 
         monome.led_on( starting[:x], y )
+        @lights_on.push( {starting[:x], y} )        
       end  
     end 
   end
